3dcd44a241fef8f5643a66e2ff4b633a9bd4a5b0
Jeann Carlos Batemarque <42376861+jeann47@users.noreply.github.com>
2020-02-13 21:03:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if(msg.content === 'hm') {
-    client.channels.get(`668073025848999950`).send(`${newMember.displayName} digite !entrar para jogar!`)
-  }
-})
 client.on('voiceStateUpdate', (oldMember, newMember, msg) => {
   let newUserChannel = newMember.voiceChannel
   let oldUserChannel = oldMember.voiceChannel
@@ -25,26 +20,8 @@ client.on('message', async msg => {
     client.channels.get(`668073025848999950`).send(`${msg.member.displayName} entrará na conversa em 10 segundos`, {tts: true})
     await sleep(10000)
     msg.member.setVoiceChannel('677643141871828992')
-    // msg.member.voiceChannel.join('677639323302101005').then(() => console.log('moved'))
   }
 })
-
-client.on('message', msg => {
-    if (msg.content === 'conn') {
-      if (msg.member.voiceChannel) {
-          msg.member.voiceChannel.join()
-            .then(connection => { // Connection is an instance of VoiceConnection
-              msg.reply('I have successfully connected to the channel!');
-            })
-            .catch(console.log);
-        } else {
-          msg.reply('You need to join a voice channel first!');
-        }
-    }
-    if (msg.content === 'say') {
-        
-    }
-});
 
 
 function sleep(ms) {
