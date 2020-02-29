c9b9c7cb3588ce7ea03c20859e7f069bf3255412
Jeann Carlos Batemarque <jeann.carlos4@gmail.com>
2020-02-29 01:03:07 -0300

    __dirname

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ client.on('message', async msg => {
     const file = msg.attachments.first();
     saveFile(
       file.url,
-      resolve(__dirname, 'waiting', `${msg.member.user.username}.mp3`)
+      resolve(process.cwd(), 'waiting', `${msg.member.user.username}.mp3`)
     );
   }
 
